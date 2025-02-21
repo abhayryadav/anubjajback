b3d5c34a8021992db31597edc828671c12ff0799
Abhay Raj Yadav <abhayryadav0007@gmail.com>
2025-02-21 19:59:51 +0530

    cmadv2

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
 
 // Enable CORS middleware
 app.use(
